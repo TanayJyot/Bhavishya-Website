edf14374d1bb1d8c3cf85f7f7faf94b054266c21
ananditavishnoi <anandita.vishnoi@gmail.com>
2020-08-16 16:42:06 +0530

    made cards for about us

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
+
 app.get("/", function(req, res) {
     res.render("home")
 })
